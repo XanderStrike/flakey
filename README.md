f57a9ce9737c8ca1503923925cbf226d8add13d6
Alex Standke <xanderstrike@gmail.com>
2024-12-05 19:11:36 -0800

    improve

---
+++ README.md
@@ -0,0 +1,18 @@
+# flaky app
+
+a test app that simulates problematic backend behaviors:
+- random status codes 
+- configurable failures
+- delays
+- memory leaks 
+- cpu spikes
+- chained requests
+- large responses
+- streaming
+
+## run it 
+
+```
+bundle install
+rackup
+```
--- flaky_app.rb
+++ flaky_app.rb
@@ -1,12 +1,124 @@
 require 'sinatra'
+require 'http'
 
 counter = 0
 
 get '/' do
-  counter += 1
-  if counter % 3 == 0
-    'hello world'
+  status_codes = [200, 201, 202, 301, 302, 400, 401, 403, 404, 500, 502, 503]
+  status status_codes.sample
+  <<~HTML
+    <h1>flaky app</h1>
+    <p>endpoints to simulate backend issues:</p>
+    
+    <ul>
+      <li><code>/</code> - random status code</li>
+      <li><code>/flaky/50</code> - fails 50% of the time</li>
+      <li><code>/delay/5</code> - 5 second delay</li>
+      <li><code>/memory/10</code> - allocate 10mb</li>
+      <li><code>/cpu/2</code> - cpu spike for 2s</li>
+      <li><code>/chain/3</code> - 3 chained requests</li>
+      <li><code>/size/1024</code> - 1mb response</li>
+      <li><code>/stream/10</code> - stream for 10s</li>
+    </ul>
+  HTML
+end
+
+get '/flaky/:failure_rate' do |failure_rate|
+  rate = failure_rate.to_i
+  if rate < 0 || rate > 100
+    status 400
+    "Failure rate must be between 0 and 100"
   else
-    status 500
+    if rand(100) < rate
+      status 500
+      "Simulated failure"
+    else
+      "Success!"
+    end
   end
 end
+
+get '/delay/:seconds' do |seconds|
+  seconds = seconds.to_f
+  if seconds < 0 || seconds > 30
+    status 400
+    "Delay must be between 0 and 30 seconds"
+  else
+    sleep seconds
+    "Waited #{seconds} seconds"
+  end
+end
+
+# Simulate memory leak
+get '/memory/:mb' do |mb|
+  mb = mb.to_i
+  if mb < 0 || mb > 100
+    status 400
+    "Memory allocation must be between 0-100 MB"
+  else
+    @data ||= []
+    @data << ' ' * (mb * 1_000_000)  # Allocate MB of memory
+    "Allocated #{mb}MB. Total allocations: #{@data.length}"
+  end
+end
+
+# Simulate CPU intensive task
+get '/cpu/:seconds' do |seconds|
+  seconds = seconds.to_f
+  if seconds < 0 || seconds > 10
+    status 400
+    "CPU time must be between 0-10 seconds"
+  else
+    end_time = Time.now + seconds
+    counter = 0
+    while Time.now < end_time
+      counter += 1
+    end
+    "Performed #{counter} iterations in #{seconds} seconds"
+  end
+end
+
+# Chain of dependent requests
+get '/chain/:depth' do |depth|
+  depth = depth.to_i
+  if depth < 0 || depth > 5
+    status 400
+    "Chain depth must be between 0-5"
+  else
+    response = "Chain #{depth}: "
+    if depth > 0
+      sleep 0.1  # Simulate some work
+      next_response = HTTP.get("http://localhost:#{request.port}/chain/#{depth - 1}").to_s
+      response += next_response
+    end
+    response
+  end
+end
+
+# Random response size
+get '/size/:kb' do |kb|
+  kb = kb.to_i
+  if kb < 0 || kb > 25600  # 25MB = 25600KB
+    status 400
+    "Size must be between 0-25600 KB (0-25MB)"
+  else
+    'x' * (kb * 1024)
+  end
+end
+
+# Simulate slow response with chunks
+get '/stream/:seconds' do |seconds|
+  seconds = seconds.to_f
+  if seconds < 0 || seconds > 30
+    status 400
+    "Stream time must be between 0-30 seconds"
+  else
+    stream do |out|
+      (seconds * 2).to_i.times do |i|
+        out << "Chunk #{i + 1}\n"
+        sleep 0.5
+      end
+    end
+  end
+end
+
